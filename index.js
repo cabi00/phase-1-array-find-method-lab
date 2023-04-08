236ad307b38e500160f83ea0fe760b52da1bf93d
vanessa garcia <venessag0908@gmail.com>
2023-04-08 01:17:06 -0600

    find function lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(broncos){
+    return broncos.find(object =>object.result === "W") ? broncos.find(object => object.result === "W").year : undefined;
+    
+}
+    /*
+ return result
+result.year : undefined
+*/
